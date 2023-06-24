8dd2bad833c329e3e64b9ac060c39df49cf5aeb0
Gaurav Jaiswal <gaurav.rcet@gmail.com>
2023-06-25 00:37:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://reqres.in/api/users?page=2`);
+        const res = await response.json();
+        setUsers(res.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredUsers = users.filter(
+    (user) =>
+      user.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.last_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Greendzine task by GJ</header>
+
+      <h1>List of users</h1>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+
+      <ul>
+        {filteredUsers.map((user) => (
+          <tr className="user-list">
+            <td key={user.id}>
+              <img src={user.avatar} alt={user.email} />
+              <div>
+                <h3>
+                  {user.first_name} {user.last_name}
+                </h3>
+                <p>{user.email}</p>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
